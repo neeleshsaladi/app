2c896c0f3f9c0e0d3128ce67f6a2e6b42b822c0d
Neelesh <neelesh.saladi@gmail.com>
2020-02-17 17:08:24 -0600

    intialize

---
+++ scriptt.js
@@ -0,0 +1,9 @@
+function Calcuate()
+{
+    var total = document.getElementById('Total').Value;
+    var rate = document.getElementById('Rate').value;
+    var calculate = document.getElementById('Calculate').value;
+    var tip = document.getElementById('Tip').value;
+    var calculae=((Total+Rate) % 5.5);
+    document.getElementById('Calculate').innerHTML = "GrandTotal = $" + cal;
+} 
