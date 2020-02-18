61cd0c044a0484ff27318be2162e4897184417ea
Neelesh <neelesh.saladi@gmail.com>
2020-02-18 00:22:52 -0600

    changed

--- scriptt.js
+++ scriptt.js
@@ -6,7 +6,7 @@ function Calculate()
     let Rate = parseFloat(document.getElementById("Rate").value);
     let Tax = 0.055;
     let Tip = (Rate*Total)/100.0;
-    let Gtotal=((Total+calctip+(Tax*Total)) );
+    let Gtotal=((Total+Tip+(Tax*Total)) );
     document.getElementById("Tip").innerHTML = "Tip: $" +Tip;
     document.getElementById("Gtotal").innerHTML = "GrandTotal: $" + Gtotal;
 } 
